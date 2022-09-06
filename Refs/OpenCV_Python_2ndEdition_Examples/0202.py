2855927196828ca3a2995a0fe3a8925d44cba576
jtwoo98 <jtwoo98@naver.com>
2022-09-06 14:32:48 +0900

    image

--- Refs/OpenCV_Python_2ndEdition_Examples/0202.py
+++ Refs/OpenCV_Python_2ndEdition_Examples/0202.py
@@ -6,5 +6,15 @@ img = cv2.imread(imageFile)
 cv2.imwrite('./data/Lena.bmp', img)
 cv2.imwrite('./data/Lena.png', img)
 cv2.imwrite('./data/Lena2.png',img, [cv2.IMWRITE_PNG_COMPRESSION, 9])
-cv2.imwrite('./data/Lena2.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 90])
+cv2.imwrite('./data/Lena2.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 5])
 
+
+
+img  = cv2.imread('./data/Lena.bmp')    # cv2.IMREAD_COLOR
+img2 = cv2.imread('./data/Lena2.jpg',0)
+
+cv2.imshow('Lena bmp',img)
+cv2.imshow('Lena png',img2)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
